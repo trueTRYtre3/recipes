29ecaf3be8857773021e9bf2beeb61fba5ccaf71
Kyle Ortega <kyleortega253@gmail.com>
2021-07-12 15:54:25 -0700

    Made navbar responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle'
 
 const Header = () => {
 
     return (
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Global Menu</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                    <NavDropdown title="Types" id="collasible-nav-dropdown">
-                        <NavDropdown.Item as={Link} to='/cuisines'>Cuisines</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to='/meals'>Meals</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to='/dishes'>Dishes</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                    <Nav.Link >Sign In</Nav.Link>
-                </Nav>
+                <Navbar.Brand href="#home">Global Food</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                        <NavDropdown title="Types" id="collasible-nav-dropdown">
+                            <NavDropdown.Item as={Link} to='/cuisines'>Cuisines</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/meals'>Meals</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/dishes'>Dishes</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link >Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar> 
         </div>
     )
