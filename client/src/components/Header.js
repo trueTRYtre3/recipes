618b7a5867bda41d4bb27e6be7991cef41203076
Kyle Ortega <kyleortega253@gmail.com>
2021-07-12 15:58:11 -0700

    Now header is responsive!!

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
 
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Navbar.Brand href="#home">Global Food</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
