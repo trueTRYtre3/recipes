e058a52845fdda0fa2379213dbb462fb72171848
Kyle Ortega <kyleortega253@gmail.com>
2021-07-12 19:14:28 -0700

    removed error

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import NavbarToggle from 'react-bootstrap/esm/NavbarToggle'
 
 const Header = () => {
 
