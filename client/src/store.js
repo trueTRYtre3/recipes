c34c38c6c953d1644cc2f9b014c6568c41cd1c08
Kyle Ortega <kyleortega253@gmail.com>
2021-07-14 23:48:50 -0700

    new redux reducer for pages

--- client/src/store.js
+++ client/src/store.js
@@ -4,11 +4,13 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import paramsReducer from './reducers/recipeParamsReducer'
 import recipeReducer from './reducers/recipeReducer'
 import foodReducer from "./reducers/foodReducer"
+import pageReducer from './reducers/paginationReducer'
 
 const reducer = combineReducers({
     params: paramsReducer,
     recipes: recipeReducer,
-    food: foodReducer
+    food: foodReducer,
+    page: pageReducer
 })
 
 const store = createStore(
