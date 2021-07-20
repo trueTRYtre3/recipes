7a694e896730e36659291c9ddaa8362b53abe4ba
Kyle Ortega <kyleortega253@gmail.com>
2021-07-19 18:56:19 -0700

    mongoose connected

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+    recipes: {
+        type: Array,
+        default: []
+    },
+    username: {
+        type: String,
+        unique: true,
+        minlength: 3,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.plugin(uniqueValidator)
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
