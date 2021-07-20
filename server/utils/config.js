254da21dfb7c65a416156d18a624c9aca1989e74
Kyle Ortega <kyleortega253@gmail.com>
2021-07-19 18:57:54 -0700

    mongodb uri added

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT
 
 const URL = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.FOOD_ID}&app_key=${process.env.FOOD_KEY}&imageSize=SMALL`
 
+const MONGODB_URI = process.env.MONGODB_URI
+
 module.exports = {
-    PORT, URL
+    PORT, URL, MONGODB_URI
 }
